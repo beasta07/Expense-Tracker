b14e8c2f9e2f8cbf240c832ef377db03e3b8a050
Pranish <pranishbista82@gmail.com>
2026-05-28 16:06:05 +0545

    i hate typescript

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 
 import 'dotenv/config'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const connectionString = process.env.DATABASE_URL
