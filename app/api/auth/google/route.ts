a0a5c8947086b2a937b944eefd83e2d7def5377d
Pranish <pranishbista82@gmail.com>
2026-05-28 15:28:24 +0545

    i hate typescript

--- app/api/auth/google/route.ts
+++ app/api/auth/google/route.ts
@@ -1,4 +1,4 @@
-export async function GET(request:Request,response:Response){
+export async function GET(request:Request){
 const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:3000/api/auth/callback/google&response_type=code&scope=profile email`
    return Response.redirect(googleAuthUrl)
 }
